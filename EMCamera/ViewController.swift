ccc82a5ee13b70b0ab422b682a99b18813c9bda1
Митько Евгений <evgenymitko@macbook-air-mitko.local>
2017-03-09 22:22:01 +0300

    Initial Commit

---
+++ EMCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EMCamera
+//
+//  Created by Митько Евгений on 09.03.17.
+//  Copyright © 2017 Evgeny Mitko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
